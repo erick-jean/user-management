81f156e5afe9932dec88244ab3e471e6703d7648
Erick Jean <erickjean36@gmail.com>
2026-07-30 08:53:59 -0400

    feat: enable CORS for API requests from frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PATCH'],
+  });
+
   // Configura a validação global dos dados recebidos pela API
   app.useGlobalPipes(
     new ValidationPipe({
