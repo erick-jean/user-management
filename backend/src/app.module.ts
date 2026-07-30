722f73da1219d6e462f2fa3a01d735c3b0664145
Erick Jean <erickjean36@gmail.com>
2026-07-30 15:07:50 -0400

    chore: remove codigo morto do projeto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
@@ -15,6 +14,5 @@ import { ConfigModule } from '@nestjs/config';
   ],
 
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
